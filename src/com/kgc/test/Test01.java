e76d6866184b21cd39b897be7a5242836bf32bc7
snym666 <1289610382@qq.com>
2018-10-07 17:06:40 +0800

    001

---
+++ src/com/kgc/test/Test01.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.kgc.test;
+
+/**
+ * @author acer
+ *
+ */
+public class Test01 {
+   public static void main(String[] args) {
+	 System.out.println("一个非常牛逼的项目");
+  }
+}
